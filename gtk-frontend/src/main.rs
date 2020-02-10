5ed2e643088cd41eeeedcee4ea50fdb47ff6a913
nzaharov <nzaharov988@gmail.com>
2020-02-11 00:01:53 +0200

    some bar experiments

--- gtk-frontend/src/main.rs
+++ gtk-frontend/src/main.rs
@@ -10,7 +10,9 @@ use gtk::{Application, ApplicationWindow, Button, Label};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use sysinfo::{ProcessorExt, SystemExt};
+use sysinfo::{ProcessorExt, System, SystemExt};
+
+type BarRefs = Rc<RefCell<Vec<gtk::LevelBar>>>;
 
 pub fn setup_system_interval(
     refresh_time: u32,
@@ -26,7 +28,7 @@ pub fn setup_system_interval(
             system.refresh_system();
             let cpu_usage =
                 system
-                    .get_processor_list()
+                    .get_processors()
                     .iter()
                     .fold(String::new(), |mut acc, processor| {
                         acc.push_str(&format!("{}: {:.2}%\n", processor.get_name(), processor.get_cpu_usage() * 100_f32));
@@ -42,14 +44,38 @@ pub fn setup_system_interval(
     );
 }
 
+pub fn setup_processors_interval(
+    refresh_time: u32,
+    system: &Rc<RefCell<sysinfo::System>>,
+    bar_refs: &BarRefs,
+) {
+    gtk::timeout_add(
+        refresh_time,
+        clone!(@strong system, @strong bar_refs => @default-return glib::Continue(true), move || {
+            let mut system = system.borrow_mut();
+            let bars = bar_refs.borrow();
+
+            system.refresh_system();
+
+            let processors = system.get_processors();
+
+            bars.iter().zip(processors.iter()).for_each(|(bar,processor)| {
+                bar.set_value(processor.get_cpu_usage() as f64);
+            });
+
+            glib::Continue(true)
+            }
+        ),
+    );
+}
+
 fn main() {
-    let application =
-        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
-            .expect("failed to initialize GTK application");
+    let application = Application::new(Some("com.nzaharov.rtop"), Default::default())
+        .expect("failed to initialize GTK application");
 
     application.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
-        window.set_title("First GTK+ Program");
+        window.set_title("rtop");
         window.set_default_size(400, 200);
 
         let button = Button::new_with_label("Click me!");
@@ -57,16 +83,34 @@ fn main() {
             println!("Clicked!");
         });
 
-        let system = sysinfo::System::new();
+        let boxy = gtk::BoxBuilder::new()
+            .orientation(gtk::Orientation::Vertical)
+            .build();
+        window.add(&boxy);
+
+        let system = System::new();
+
+        let bars: Vec<gtk::LevelBar> = system
+            .get_processors()
+            .iter()
+            .map(|processor| {
+                let builder = gtk::LevelBarBuilder::new();
+                let bar = builder.min_value(0_f64).max_value(100_f64).build();
+                boxy.add(&bar);
+
+                bar
+            })
+            .collect();
+
         let system = Rc::new(RefCell::new(system));
+        let bar_refs = Rc::new(RefCell::new(bars));
+        // let label = Label::new_with_mnemonic(Some("&cpu_usage"));
+        // window.add(&label);
+        // let label_refc = Rc::new(RefCell::new(label));
 
-        let label = Label::new_with_mnemonic(Some("&cpu_usage"));
-        window.add(&label);
-        let label_refc = Rc::new(RefCell::new(label));
+        setup_processors_interval(1000, &system, &bar_refs);
 
-        setup_system_interval(1000, &system, &label_refc);
-
-        println!("{:?}", system);
+        // println!("{:?}", system);
 
         // window.add(&button);
 
