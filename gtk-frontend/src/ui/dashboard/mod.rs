9a8e8f0d703c09ca11cd14a866e27a68067f34fe
nzaharov <nzaharov988@gmail.com>
2020-02-13 19:56:17 +0200

    upper grid added

--- gtk-frontend/src/ui/dashboard/mod.rs
+++ gtk-frontend/src/ui/dashboard/mod.rs
@@ -15,9 +15,19 @@ impl Dashboard {
             .orientation(gtk::Orientation::Vertical)
             .expand(true)
             .build();
+
+        let upper_container = gtk::GridBuilder::new().expand(true).baseline_row(0).build();
         let processors = Processors::new(8);
-        let temp_label = gtk::LabelBuilder::new().label("Temp label").build();
-        container.add1(&processors.container);
+        upper_container.attach(&processors.container, 0, 0, 2, 1);
+
+        let temp_label1 = gtk::LabelBuilder::new().label("Memory placeholder").build();
+        upper_container.attach(&temp_label1, 0, 1, 1, 1);
+
+        let temp_label2 = gtk::LabelBuilder::new().label("Usage placeholder").build();
+        upper_container.attach(&temp_label2, 1, 1, 1, 1);
+        
+        let temp_label = gtk::LabelBuilder::new().label("Processes placeholder").build();
+        container.add1(&upper_container);
         container.add2(&temp_label);
 
         Dashboard {
